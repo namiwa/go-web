8610ea0f73082b5c1c56ae279f048c53e08acc5e
namiwa <khairul.nasri895@gmail.com>
2024-03-15 05:34:20 +0800

    feat: use flags package for cli flags

--- frontend/main.go
+++ frontend/main.go
@@ -1,74 +1,80 @@
 package main
 
 import (
+	"flag"
 	"os"
 )
 
-func build() {
-  infoLog("Starting main markdown parser")
-  source := getFromArray(os.Args, 2)
-  if source == nil {
-    infoLog("missing source directory")
-    os.Exit(1)
-  }
-  target := getFromArray(os.Args, 3)
-  if target == nil {
-    infoLog("missing target directory")
-    os.Exit(1)
-  }
-  err := buildHtmlDirFromSource(*source, *target, true)
-  if err == nil {
-    infoLog("successfully converted markdown to html")
-    os.Exit(0)
-  } else {
-    infoLog("failed to read files")
-    os.Exit(1)
-  }
+func build(source *string, target *string) {
+	infoLog("Starting main markdown parser")
+	err := buildHtmlDirFromSource(*source, *target, true)
+	if err == nil {
+		infoLog("successfully converted markdown to html")
+		os.Exit(0)
+	} else {
+		infoLog("failed to read files")
+		os.Exit(1)
+	}
 }
 
-func serve() {
-  infoLog("starting webserver")
-  target := getFromArray(os.Args, 2)
-  if target == nil {
-    infoLog("missing target directory")
-    os.Exit(1)
-  }
-  startServer(*target)
-  os.Exit(0)
+func serve(target *string) {
+	infoLog("starting webserver")
+	if *target == "" {
+		infoLog("empty target directory")
+		os.Exit(1)
+	}
+	startServer(*target)
+	os.Exit(0)
 }
 
-func buildServe() {
-  infoLog("starting build webserver")
-  target := getFromArray(os.Args, 2)
-  if target == nil {
-    infoLog("missing target directory")
-    os.Exit(1)
-  }
-  buildServer(*target)
-  os.Exit(0)
+func buildServe(target *string) {
+	infoLog("starting build webserver")
+	if *target == "" {
+		infoLog("empty target directory")
+		os.Exit(1)
+	}
+	buildServer(*target)
+	os.Exit(0)
 }
 
 func main() {
-  cmd := getFromArray(os.Args, 1)
-  if cmd == nil {
-    infoLog("Unknown command, try build, serve or buildServe")
-    os.Exit(1)
-  }
-  switch (*cmd) {
-    case "build": {
-      build()
-    }
-    case "serve": {
-      serve()
-    }
-    case "buildServe": {
-      buildServe()
-    }
-    default: {
-      infoLog("Error, unknown command", cmd)
-      os.Exit(1)
-    }
-  }
+	cmd := flag.String("cmd", "", "go-web command: build, serve or buildServe")
+	source := flag.String("source", "", "source directory of markdown")
+	target := flag.String("target", "", "target output directory of htmls")
+	flag.Parse()
+	switch *cmd {
+	case "build":
+		{
+			if *source == "" {
+				infoLog("empty source directory")
+				os.Exit(1)
+			}
+			if *target == "" {
+				infoLog("empty target directory")
+				os.Exit(1)
+			}
+			build(source, target)
+		}
+	case "serve":
+		{
+			if *target == "" {
+				infoLog("empty target directory")
+				os.Exit(1)
+			}
+			serve(target)
+		}
+	case "buildServe":
+		{
+			if *target == "" {
+				infoLog("empty target directory")
+				os.Exit(1)
+			}
+			buildServe(target)
+		}
+	default:
+		{
+			infoLog("Error, unknown command", cmd)
+			os.Exit(1)
+		}
+	}
 }
-
-
